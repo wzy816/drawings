555fed8fda9ab263ed0346ee498ba0fec9898ff6
zhiyang.wang <wzy816@gmail.com>
2021-09-26 20:55:04 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # drawings
 
 architecture image collection
+
+![icok_icok05_ma_li_3d_640](<./industrial_design/icok_icok05_ma_li_3d_640.jpg>)
+![Circle-Collect_p1241844918](<./illustration/Circle-Collect_p1241844918.jpg>)
+![Sin_Centre_-_Michel_Webb_(Archigram)._1962_(1)](<./project_multiple/Sin_Centre_-_Michel_Webb_(Archigram)._1962_(1).jpg>)
+![icok_icok03_playstation-best-controversial-ads-35-560x1-thumb](<./industrial_design/icok_icok03_playstation-best-controversial-ads-35-560x1-thumb.jpg>)
--- gen.py
+++ gen.py
@@ -1,8 +1,11 @@
 import os
 import glob
+import random
 
-# regenerate README.md file for all subfolder
+# regenerate README.md file for all subfolders and root folder
 # python3 gen.py
+
+imgs = []
 for file in os.listdir("./"):
     if os.path.isdir(os.path.join("./", file)):
         folder_name = os.path.basename(file)
@@ -12,3 +15,15 @@ for file in os.listdir("./"):
                 bn = os.path.basename(img)
                 f.write("!["+os.path.splitext(bn)[0]+"](<"
                         + bn+">)"+"\n")
+                imgs.append(img)
+            f.close()
+random.shuffle(imgs)
+
+with open("./README.md", "w+") as f:
+    f.write("# drawings\n\n")
+    f.write("architecture image collection\n\n")
+    for i in range(4):
+        bn = os.path.basename(imgs[i])
+        f.write("!["+os.path.splitext(bn)[0]+"](<"
+                + imgs[i]+">)"+"\n")
+    f.close()
